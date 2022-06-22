6a80102befb36709c59589e19fb9b8aaa7b40f2d
vijay-kartik <kartik.iit96@gmail.com>
2022-06-23 02:15:33 +0530

    second commit

--- TypingSpeedTestApp/index.js
+++ TypingSpeedTestApp/index.js
@@ -1,11 +1,39 @@
 const inputElement = document.querySelector('input');
 const messageArea = document.querySelector(".message");
+const restartBtn = document.querySelector('button');
+const errorElement = document.querySelector('#errors-text');
+const accuracyElement = document.querySelector('#accuracy-text');
 const dummyText = "The journey of a thousand miles begins with a single step.";
+const hintMessage = "Click the area below to start the game."
 const colors = ["black", "green", "red"];
+let timerId = -1;
+let timePassed = 0;
+let errorCount = 0;
 
+const timedText = document.getElementById("time-text");
 inputElement.addEventListener('input', updateValue);
 
-inputElement.addEventListener('focus', changeMessageText);
+inputElement.addEventListener('focus', handleTypingAreaFocus);
+
+restartBtn.addEventListener('click', resetParams);
+
+function handleTypingAreaFocus(event) {
+    changeMessageText(event);
+    timePassed=0;
+    errorCount = 0;
+    timedText.innerText = (60-timePassed).toString() + 's';
+    startTimer();
+}
+
+function startTimer() {
+    timerId = setInterval(() => {
+        timePassed++;
+        timedText.innerText = (60-timePassed).toString() + 's';
+        if(timePassed === 60) {
+            clearInterval(timerId);
+        }
+    }, 1000);
+}
 
 function changeMessageText(event) {
     let messageSpan = document.querySelector("#message-text");
@@ -21,11 +49,17 @@ function appendTextNode(textString, colorType, fontSize) {
         textNode.style.color = colors[colorType];
         textNode.style.fontSize = fontSize;
         messageArea.appendChild(textNode);
+        if(colorType == 2) {
+            errorCount += textString.length;
+            errorElement.innerText = errorCount;
+        }
+
     }
 }
 
 function updateValue(event) {
     messageArea.innerHTML = "";
+    errorCount = 0;
     let inputValue = event.target.value;
     let inputLen = inputValue.length;
     let textLen = dummyText.length;
@@ -34,7 +68,7 @@ function updateValue(event) {
     let currentText = "";
     let currentType = 0;
     while(i < inputLen && i < textLen ) {
-        if(inputValue.charAt(i) == dummyText.charAt(i)) {
+        if(inputValue.charAt(i) === dummyText.charAt(i)) {
             if(currentType != 1) {
                 appendTextNode(currentText, currentType, '15px');
                 currentType = 1;
@@ -59,5 +93,17 @@ function updateValue(event) {
         i++;
     }
     appendTextNode(currentText, currentType, '15px');
+    accuracyElement.innerText = Math.floor((1 - errorCount/inputLen)*100);
+    
+}
+
+function resetParams() {
+    timePassed=0;
+    timedText.innerText = '60s';
+    messageArea.innerText = hintMessage;
+    inputElement.innerText = '';
+    errorCount = 0;
+    errorElement.innerText = 0;
+    accuracyElement.innerText = 100;
 }
 
